540b8d6f02a813ebf3d63ed0e4c87998a53b51dd
Vincibean <andrebessi00@gmail.com>
2018-04-19 23:01:27 +0000

    added new solutions for all-even.hs

--- haskell/day-1/all-even.hs
+++ haskell/day-1/all-even.hs
@@ -2,3 +2,9 @@ module AllEven where
   allEven :: [Integer] -> [Integer]
   allEven [] = []
   allEven (h: t) = if even h then h: allEven t else allEven t
+
+  allEven2 :: [Integer] -> [Integer]
+  allEven2 is = [x | x <- is, even x]
+
+  allEven3 :: [Integer] -> [Integer]
+  allEven3 = filter even
