2b075212c68c4bb0337e454e3e68dd26f068ff4e
Vincibean <andrebessi00@gmail.com>
2018-04-19 23:17:08 +0000

    code formatting

--- haskell/day-1/all-even.hs
+++ haskell/day-1/all-even.hs
@@ -1,7 +1,7 @@
 module AllEven where
   allEven :: [Integer] -> [Integer]
   allEven [] = []
-  allEven (h:t) = if even h then h: allEven t else allEven t
+  allEven (h:t) = if even h then h:allEven t else allEven t
 
   allEven2 :: [Integer] -> [Integer]
   allEven2 is = [x | x <- is, even x]
