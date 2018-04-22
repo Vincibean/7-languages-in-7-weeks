532b74fb5624dd08efa046c5d118f07720553280
Vincibean <andrebessi00@gmail.com>
2018-04-22 23:38:29 +0000

    added greatest common denominator module

---
+++ haskell/day-2/gcd.hs
@@ -0,0 +1,6 @@
+module GCD where
+
+  -- using Euclid's algorithm (interestingly, it's a 1:1 mapping with the mathematical definition provided by Wikipedia)
+  gcd' :: Int -> Int -> Int
+  gcd' a 0 = a
+  gcd' a b = gcd b (a `mod` b)
