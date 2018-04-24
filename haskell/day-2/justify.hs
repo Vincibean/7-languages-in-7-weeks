fbdf022ecb256279f430985ccbb5cb96762129ba
Vincibean <andrebessi00@gmail.com>
2018-04-24 16:20:54 +0000

    improved justify.hs

--- haskell/day-2/justify.hs
+++ haskell/day-2/justify.hs
@@ -1,8 +1,13 @@
 module Justify where 
   import BreakLinesNumbers
 
-  rightJustify :: [(Int, String)] -> [(Int, String)]
-  rightJustify ts = map (\(f,s) -> (f,rj mx s)) ts
+  data Justification = JLeft | JRight | JFull deriving (Eq)
+
+  justify :: Justification -> [(Int, String)] -> [(Int, String)]
+  justify just ts = map (\(f,s) -> (f,rj just mx s)) ts
     where mx = maximum $ map (length . snd) ts
-          rj mx s = (replicate toRep ' ') ++ s
-            where toRep = mx - length s
+          rj just mx s 
+           | just == JRight = (replicate toRep ' ') ++ s
+           | just == JLeft = s ++ (replicate toRep ' ')
+           | just == JFull = let rep = replicate (quot toRep 2) ' ' in rep ++ s ++ rep
+           where toRep = mx - length s
