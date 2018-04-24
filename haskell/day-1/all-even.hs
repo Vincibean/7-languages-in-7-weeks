59bba9c9b7c20cf28cfaa7635e5bdac387dcc1a4
Vincibean <andrebessi00@gmail.com>
2018-04-24 20:11:39 +0000

    improved all-even.hs and reverse.hs

--- haskell/day-1/all-even.hs
+++ haskell/day-1/all-even.hs
@@ -3,13 +3,13 @@ module AllEven where
   allEven [] = []
   allEven (h:t) = if even h then h:allEven t else allEven t
 
-  allEven2 :: [Integer] -> [Integer]
-  allEven2 is = [x | x <- is, even x]
+  allEven' :: [Integer] -> [Integer]
+  allEven' is = [x | x <- is, even x]
 
-  allEven3 :: [Integer] -> [Integer]
-  allEven3 = filter even
+  allEven'' :: [Integer] -> [Integer]
+  allEven'' = filter even
 
-  allEven4 :: [Integer] -> [Integer]
-  allEven4 = reverse . allEvenAcc []
+  allEven''' :: [Integer] -> [Integer]
+  allEven''' = reverse . allEvenAcc []
     where allEvenAcc acc [] = acc  
           allEvenAcc acc (h:t) = if even h then allEvenAcc (h:acc) t else allEvenAcc acc t 
